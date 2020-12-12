825a9e4ee53a3a73301669ce876bfdbc312869b5
Rafiul Islam <avoidcloud@gmail.com>
2020-12-12 15:22:51 +0600

    subscription and authentication dto

---
+++ SenseHome.DataTransferObjects/Authentication/UserLoginDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SenseHome.DataTransferObjects.Authentication
+{
+    public class UserLoginDto
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+    }
+}
---
+++ SenseHome.DataTransferObjects/Subscription/SubscriptionInsertDto.cs
@@ -0,0 +1,10 @@
+﻿using SenseHome.DataTransferObjects.Base;
+
+namespace SenseHome.DataTransferObjects.Subscription
+{
+    public class SubscriptionInsertDto : BaseDtoWithLog
+    {
+        public string[] Path { set; get; }
+        public string UserId { get; set; }
+    }
+}
