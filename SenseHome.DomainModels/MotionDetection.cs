c361503c3eb12925317f5898485ef485e144b109
Rafiul Islam <avoidcloud@gmail.com>
2020-12-12 15:55:40 +0600

    added temperature-humidity and motion detection domain models and packing the domain project

---
+++ SenseHome.DomainModels/MotionDetection.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using SenseHome.DomainModels.Base;
+
+namespace SenseHome.DomainModels
+{
+    public class MotionDetection : BaseEntity
+    {
+        public DateTime Date { get; set; }
+    }
+}
---
+++ SenseHome.DomainModels/TemperatureHumidity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SenseHome.DomainModels.Base;
+
+namespace SenseHome.DomainModels
+{
+    public class TemperatureHumidity : BaseEntity
+    {
+        public string Topic { get; set; }
+        public double Temperature { get; set; } // Celsius format
+        public double Humidity { get; set; } // % format
+        public DateTime Date { get; set; }
+    }
+}
