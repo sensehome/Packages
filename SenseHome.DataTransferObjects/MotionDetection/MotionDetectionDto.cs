cb89f2e39d7d1a276557ee8f47127a2fac892def
Rafiul Islam <avoidcloud@gmail.com>
2020-12-16 15:35:12 +0600

    added temperature-humidity and motion detection dto

---
+++ SenseHome.DataTransferObjects/MotionDetection/MotionDetectionDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using SenseHome.DataTransferObjects.Base;
+
+namespace SenseHome.DataTransferObjects.MotionDetection
+{
+    public class MotionDetectionDto : BaseDto
+    {
+        public DateTime Date { get; set; }
+    }
+}
---
+++ SenseHome.DataTransferObjects/TemperatureHumidity/TemperatureHumidityDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SenseHome.DataTransferObjects.Base;
+
+namespace SenseHome.DataTransferObjects.TemperatureHumidity
+{
+    public class TemperatureHumidityDto : BaseDto
+    {
+        public string Topic { get; set; }
+        public double Temperature { get; set; } // Celsius format
+        public double Humidity { get; set; } // % format
+        public DateTime Date { get; set; }
+    }
+}
