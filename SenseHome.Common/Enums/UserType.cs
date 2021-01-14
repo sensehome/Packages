c8dc0b3a496b2f57a672498dda1578a28b4257ea
Rafiul Islam <avoidcloud@gmail.com>
2021-01-14 10:43:32 +0600

    enum type display name get extension method

--- SenseHome.Common/Enums/UserType.cs
+++ SenseHome.Common/Enums/UserType.cs
@@ -1,9 +1,14 @@
-﻿namespace SenseHome.Common.Enums
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SenseHome.Common.Enums
 {
     public enum UserType
     {
+        [Display(Name = "Admin")]
         Admin = 1,
+        [Display(Name = "User")]
         User,
+        [Display(Name = "Appliances")]
         Appliances
     }
 }
